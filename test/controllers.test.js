caf0408e14e46380e2abb9980447b8355bf7392a
abianco3 <anthony.bianco3@gmail.com>
2017-04-08 16:06:32 -0700

    Update controllers.test.js

--- test/controllers.test.js
+++ test/controllers.test.js
@@ -1,6 +1,6 @@
+process.env.NODE_ENV = "TESTING";
 const supertest = require('supertest');
 const app = require('../server/server.js');
-process.env.NODE_ENV = "TESTING";
 
 test('it should be handled by the auctions controller', () => {
   return supertest(app)
