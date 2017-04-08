1213343d238a472c23e5272be99f474ccb2d41eb
Alison Zhang <alisonzthu@gmail.com>
2017-04-08 15:06:03 -0700

    simple hardcoded homepage

--- client/src/components/LoggedOutNav.jsx
+++ client/src/components/LoggedOutNav.jsx
@@ -1,49 +1,32 @@
 import React, { Component } from 'react';
-import { browserHistory } from 'react-router-dom';
+import { Link, browserHistory } from 'react-router-dom';
 // import { Grid, Icon, Header, Input, Menu, Segment } from 'semantic-ui-react';
 
 class LoggedOutNav extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {activeItem: 'home'};
-		this.handleItemClick =(e, { name }) => {
-			this.setState({activeItem: name});
-			
-			// browserHistory.push('/auctions');
-	    };
 	}
 
 	render() {
-		const { activeItem } = this.state;
 
 		return (
-			<Grid>
-			  <Grid.Column width={6} verticalAlign="middle">
-			    <Header as="h1" textAlign="center"> 
-			    Art Point
-			    </Header>
-			  </Grid.Column>
-			  <Grid.Column>
-			    <Segment inverted>
-			      <Menu inverted secondary>
-			        <input placeholder="search" type="text" />
-			        <Menu.Item name="home" active={activeItem === "home"} onClick={this.handleItemClick} />
-			        {'  '}
-			        <Menu.Item name="auctions" active={activeItem === "auctions"} onClick={this.handleItemClick} />
-			        {'  '}
-			        <Menu.Item name="artists" active={activeItem === "artists"} onClick={this.handleItemClick} />
-			        {'  '}
-			        <Menu.Item name="events" active={activeItem === "events"} onClick={this.handleItemClick} />
-			        {'  '}
-			        <Menu.Item name="contact us" active={activeItem === "contact us"} onClick={this.handleItemClick} />
-			        {'  '}
-			        <Menu.Item name="log in" active={activeItem === "log in"} onClick={this.handleItemClick} />
-			        {'  '}
-			        <Menu.Item name="sign up" active={activeItem === "sign up"} onClick={this.handleItemClick} />
-			      </Menu>
-			    </Segment>
-			  </Grid.Column>
-			</Grid>
+			<div>
+			  <input type="text" placeholder="search" />
+			  {'  '}
+			  <Link to="/">Home</Link>
+			  {'  '}
+			  <Link to="/auctions">Auctions</Link>
+			  {'  '}
+			  <Link to="/artists">Artists</Link>
+			  {'  '}
+			  <Link to="/events">Events</Link>
+			  {'  '}
+			  <Link to="/contact">Contact us</Link>
+			  {'  '}
+			  <Link to="/login">Log In</Link>
+			  {'  '}
+			  <Link to="/signup">Sign Up</Link>
+			</div>
 
 			);
 	}
