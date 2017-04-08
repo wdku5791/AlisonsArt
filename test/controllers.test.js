2aa6abfabf79ff95848d66d85be875b4cfdc6222
Winston Ku <wdku5791@gmail.com>
2017-04-08 15:54:13 -0700

    put process.env.node_env to the top of controllers test

--- test/controllers.test.js
+++ test/controllers.test.js
@@ -1,5 +1,6 @@
 const supertest = require('supertest');
 const app = require('../server/server.js');
+process.env.NODE_ENV = "TESTING";
 
 test('it should be handled by the auctions controller', () => {
   return supertest(app)
