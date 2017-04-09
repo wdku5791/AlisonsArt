703999f1a37b0e2b5fb8602f5a7285e7dbf3b76c
Xueying Zhang <alisonzthu@gmail.com>
2017-04-09 14:35:31 -0700

    Update index.jsx

--- client/src/reducers/index.jsx
+++ client/src/reducers/index.jsx
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 //change these two reducers to ours
 import auctionReducer from './auctionReducer.jsx';
-const todoApp = combineReducers({auctionReducer});
+const todoApp = combineReducers({ auctionReducer });
 
 export default todoApp;
