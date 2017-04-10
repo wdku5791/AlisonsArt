28f4b58dfc685918449c02ffc794bc219fe663eb
Anthony Bianco <anthony.bianco3@gmail.com>
2017-04-10 15:14:13 -0700

    handle post to '/auctions' by creating an auction

--- server/controllers/auctions.js
+++ server/controllers/auctions.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
+const model = require('../database/queries');
 
 router.get('/', (req, res) => {
   res.status(200).send('you found the auctions path');
+});
+
+router.post('/', (req, res) => {
+  model.createArtwork(req.body.artwork)
+  .then((data) => {
+    req.body.artwork_id = data.id;
+    model.createAuction(req.body)
+    .then((auctionId) => {
+      res.status(201).json(auctionId);
+    });
+  })
+  .catch((err) => {
+    res.status(500).send(err);
+  });
 });
 
 module.exports = router;
