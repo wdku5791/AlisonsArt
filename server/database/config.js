d2c650accc393d9507acc85c38fa1e6c60cc9243
boybutcher <BYRONdeLARGE@yahoo.com>
2017-04-10 20:49:23 -0700

    created insertDummy function.

--- server/database/config.js
+++ server/database/config.js
@@ -1,10 +1,10 @@
 const pgp = require('pg-promise')();
 pgp.pg.defaults.ssl = true;
 const createSchema = require('./schema.js');
+const insertDummyData = require('./dummyUpload.js');
 
 const url = process.env.NODE_ENV === 'TESTING' ? process.env.TESTING_DATABASE_URL : process.env.DATABASE_URL;
 
 let db = pgp(url);
-//createSchema(db);
 
 module.exports = db;
