f0d0d4909f18933a112ab6be9fc20508473860d6
Alison Zhang <alisonzthu@gmail.com>
2017-04-11 17:13:38 -0700

    change auctions response back

--- server/controllers/auctions.js
+++ server/controllers/auctions.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const model = require('../database/queries');
 
 router.get('/', (req, res) => {
-  // res.status(200).send('you found the auctions path');
+  res.status(200).send('you found the auctions path');
   //new:
-  let s = JSON.stringify([{auction: {id: 1,name: "hey", year: 1987, painter: "Yo"}}]);
-  console.log("s: ", s);
-  res.status(200).send(s);
+  // let s = JSON.stringify([{auction: {id: 1,name: "hey", year: 1987, painter: "Yo"}}]);
+  // console.log("s: ", s);
+  // res.status(200).send(s);
 });
 
 router.post('/', (req, res) => {
