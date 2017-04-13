372f5c9a14863690c98f8a1e3b808bcf7b5173b9
Alison Zhang <alisonzthu@gmail.com>
2017-04-12 22:22:26 -0700

    in the middle of working on login post

--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
+
+//when login success, needs to save info in userReducer
 class LogIn extends Component {
+
+  _handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.usernameNode.value);
+    console.log(this.passwordNode.value);
+    this.usernameNode.value = '';
+    this.passwordNode.value = '';
+
+    fetch()
+  }  
+
   render(){
     return(
-      <form>
+      <form onSubmit={e => {this._handleSubmit(e)}}>
         Username:
-        <input type="text" placeholder="username" />
+        <input type="text" placeholder="username" ref={node => this.usernameNode = node} />
         <br />
         Password:
-        <input type="password" placeholder="password" />
-        <input type="submit" />
+        <input type="password" placeholder="password" ref={node => this.passwordNode = node}/>
+        <input type="submit"/>
       </form>
     )
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  }
+}
 
-export default LogIn;
+export default connect(mapStateToProps)(LogIn);
