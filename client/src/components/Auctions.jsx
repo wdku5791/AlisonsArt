99fcaf3c19f59b757a3a9288d1a14393a48c0a6d
Anthony Bianco <anthony.bianco3@gmail.com>
2017-04-13 14:33:27 -0700

    fetch auctions from server, use auctions reducer

--- client/src/components/Auctions.jsx
+++ client/src/components/Auctions.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import * as actions from '../actions/auctionActionCreator.jsx';
 
 import { connect } from 'react-redux';
 
 class Auctions extends React.Component {
   constructor(props) {
     super(props);
-  };
+  }
+
+  componentWillMount() {
+    const { dispatch } = this.props;
+    dispatch(actions.fetchingAuctions(true));
+
+    fetch('/auctions')
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(response.json());
+      } else {
+        dispatch(actions.fetchingAuctions(false));
+        response.json()
+        .then((auctions) => {
+          dispatch(actions.ongoingAuctionsFetchedSuccess(auctions));
+        });
+      }
+    })
+    .catch((err) => {
+      dispatch(actions.fetchAuctionErrored(true, err));
+    });
+  }
 
   render() {
     const { auctions } = this.props;
@@ -23,7 +45,7 @@ class Auctions extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-  return { auctions: state.auctions };
+  return { auctions: state.auctions.fetchedOngoingAuctions };
 };
 
 export default connect(mapStateToProps)(Auctions);
