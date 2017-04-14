818d0c680d9536fb160752aa8e2f55fe25173e34
Alison Zhang <alisonzthu@gmail.com>
2017-04-14 16:35:13 -0700

    added components into artist profile page

--- client/src/components/Artist.jsx
+++ client/src/components/Artist.jsx
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Container, Image, Grid } from 'semantic-ui-react';
 
+
+const CurrentAuctions = () => {
+  return (
+    <div>
+    im current auctions
+    </div>
+  );
+}
+
+const PreviousAuctions = () => {
+  return (
+    <div>
+    im previous auctions
+    </div>
+  );
+}
 
 class Artist extends Component {
 
   componentWillMount() {
     //fetch all data about this artist
   }
-
   
   render(){
-    console.log('heello?');
     console.log('artist id: ', this.props.match.params.artistId);
   // let auction = this.props.auction.auction;
   //need to get the buyout price of this piece and the current bidding price of the piece, then generate values for the dropbox.
@@ -19,11 +34,36 @@ class Artist extends Component {
     //send a backend request to get the data for this page
     // let artist = fetchArtist(this.props.match.params.artistId);
     return (
-      <div>
-       yoyoyoyoyoyoyoyo
-        <img />
-        <p></p>
-      </div>
+      <Container>
+        <Container>
+          <div>
+            <span>artist name</span>
+            {' '}
+            <button>Direct message</button>
+            <button>fb</button>
+            <button>twitter</button>
+            <button>instagram</button>
+          </div>
+          <div>
+            <button>prev</button>
+            <img src="./assets/temp.png" />
+            <button>next</button>
+            <textarea rows="4" cols="20" />
+          </div>
+        </Container>
+        <Grid columns={2}>
+          <Grid.Row>
+            <Grid.Column>
+              Ongoing auctions:
+              <CurrentAuctions />
+            </Grid.Column>
+            <Grid.Column>
+              Previous auctions:
+              <PreviousAuctions />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
     )
   }
 }
@@ -34,4 +74,3 @@ const mapStateToProps = (state) => {
   }
 }
 export default connect(mapStateToProps)(Artist);
-// export default Artist;
