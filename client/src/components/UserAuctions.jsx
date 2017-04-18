d8e73be479b7302d3188399812cef271d6b45334
Anthony Bianco <anthony.bianco3@gmail.com>
2017-04-17 17:26:12 -0700

    Use UserAuctionActions for fetching instead of UserActions

--- client/src/components/UserAuctions.jsx
+++ client/src/components/UserAuctions.jsx
@@ -9,20 +9,22 @@ class UserAuctions extends Component {
   constructor(props) {
     super(props);
   }
-  
+
   componentWillMount() {
-    const { dispatch, user } = this.props;
-    dispatch(userAuctions.fetchingAnAuction(true));
+    const { dispatch, user, } = this.props;
+    const { userId } = this.props.match.params;
+
+    dispatch(userAuctions.fetchingUserAuctions(true));
 
     fetch('/auctions/ongoing', {
       method: 'POST',
-      body: JSON.stringify({user: user})
+      body: JSON.stringify({user: userId})
     })
     .then(response => {
       if(!response.ok) {
         throw Error(response.statusText);
       }
-        dispatch(userAuctions.fetchingAnAuction(false));
+        dispatch(userAuctions.fetchingUserAuctions(false));
         return response.json();
     })
     .then(data => {
@@ -32,32 +34,28 @@ class UserAuctions extends Component {
   }
 
   render() {
-  //   const auction = this.props.closedAuctions[0];
+    const auction = this.props.closedAuctions[0];
 
 
-  //   if (auction.won) {
-  //     let message = <button>Send Payment</button>;
-  //   } else {
-  //     let message = <button>More by this Artist</button>;
-  //   }
-  //   return (
-  //     <Container>
-  //       <Container className="ui medium images">
-  //         <Image className="ui image" src={auction.image_url} />
-  //       </Container>
-  //       <Container>
-  //         <p>Description: {auction.description}</p>
-  //         <p>Year: {auction.artwork.age}</p>
-  //         <p>Closing Price ($USD): {auction.current_bid}</p>
-  //         {message}
-  //       </Container>
-  //     </Container>
-  //   );
-  // }
-  return (
-    <div>Auctions</div>
-  );
-}
+    if (auction.won) {
+      let message = <button>Send Payment</button>;
+    } else {
+      let message = <button>More by this Artist</button>;
+    }
+    return (
+      <Container>
+        <Container className="ui medium images">
+          <Image className="ui image" src={auction.image_url} />
+        </Container>
+        <Container>
+          <p>Description: {auction.description}</p>
+          <p>Year: {auction.artwork.age}</p>
+          <p>Closing Price ($USD): {auction.current_bid}</p>
+          {message}
+        </Container>
+      </Container>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
