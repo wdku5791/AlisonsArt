ecc84ffd095b9c92ba27415e52954c47ef667752
Winston Ku <wdku5791@gmail.com>
2017-04-19 16:34:48 -0700

    revert

--- server/server.js
+++ server/server.js
@@ -23,14 +23,6 @@ const server = app.listen(port, function() {
   console.log('Listening on port ', port);
 });
 const io = require('./sockets.js').init(server);
-module.exports = io;
-const auctionHandler = require('./controllers/auctions');
-const homeHandler = require('./controllers/home');
-const loginSignupHandler = require('./controllers/loginSignup');
-const userHandler = require('./controllers/user');
-const artistHandler = require('./controllers/artist');
-const imageHandler = require('./controllers/imageUpload');
-const notificationHandler = require('./controllers/notification');
 // app.use(bodyParser.urlencoded({extended: true}));
 // app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/../client/public')));
