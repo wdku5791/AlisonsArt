440b3817a9718a9267086ae649d4b4c831cb51ca
Alison Zhang <alisonzthu@gmail.com>
2017-04-20 08:59:28 -0700

    flushed db to get things back to work

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -6,21 +6,16 @@ const time = new Moment().format('YYYY-MM-DD HH:mm:ss');
 
 router.get('/', (req, res) => {
   const queries = [model.getAuctions(3, time, '>'), model.getAuctions(3, time, '<'), model.featuredArt()];
+
   Promise.all(queries)
-  .then((fufilled) => {
-    console.log('ahhh')
+  .then(fufilled => {
     const data = {};
-    console.log('1')
     data.current = fufilled[0];
-    console.log('2')
     data.expired = fufilled[1];
-    console.log('3')
     data.featuredArt = fufilled[2];
-    console.log('here');
     res.status(200).json(data);
   })
-  .catch((err) => {
-    console.log('ohohoh')
+  .catch(err => {
     res.status(500).send(err);
   });
 });
