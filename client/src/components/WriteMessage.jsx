8dbf0ee165ebfe8b50884770e9c0b6682b437fef
boybutcher <BYRONdeLARGE@yahoo.com>
2017-04-20 11:56:53 -0700

    able to grab appropriate messages the used sender/receiver

--- client/src/components/WriteMessage.jsx
+++ client/src/components/WriteMessage.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Segment, Input, Button } from 'semantic-ui-react';
+import { Segment, Button, TextArea} from 'semantic-ui-react';
 
 class WriteMessage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: 'this is a test message from bryan to anthony',
+      text: 'this is a message from bryan',
       sender_id: 4,
       receiver_id: 3,
+      messages: [],
     }
     this.handleFormChange = this.handleFormChange.bind(this);
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.retrieveMessages = this.retrieveMessages.bind(this);
   }
 
   handleFormChange(e) {
@@ -45,18 +47,60 @@ class WriteMessage extends React.Component {
     })
   }
 
+  retrieveMessages() {
+    fetch(`/messages/${this.state.sender_id}?receiver_id=${this.state.receiver_id}`, {
+      method: 'GET',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      })
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw Error('failed to retrieve messages...')
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log('messages retrieved! data: ', data);
+      this.setState({
+        messages: data
+      })
+      console.log('this.state.messages: ', this.state.messages);
+    })
+    .catch((error) => {
+      console.log('retrieveMessages failed! error: ', error);
+    })
+  }
+
+  componentWillMount() {
+    this.retrieveMessages()
+  }
+
   render() {
     return (
-      <Segment className='messageBar'>
-        <Input 
-          value={this.state.text}  
-          onChange={this.handleFormChange}
-        />
-        <Button 
-          onClick={this.handleFormSubmit}
-        >
-          Send
-        </Button>
+      <Segment className='messageWindow'>
+        <Segment className='messageFeed'>
+          {
+            this.state.messages.map(message => {
+              if (Number(message.sender_id) === this.state.sender_id) {
+                return <p className='senderMessage'>{message.text}</p>
+              } else {
+                return <p className='receiverMessage'>{message.text}</p>
+              }
+            })
+          }
+        </Segment>
+        <Segment className='messageInput'>
+          <TextArea autoHeight 
+            value={this.state.text}  
+            onChange={this.handleFormChange}
+          />
+          <Button 
+            onClick={this.handleFormSubmit}
+          >
+            Send
+          </Button>
+        </Segment>
       </Segment>
     )
   }
--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -4,16 +4,24 @@ const Moment = require('moment');
 
 router.post('/', (req, res) => {
   req.body.message_date = new Moment().format('YYYY-MM-DD HH:mm:ss')
-  console.log('req.body: ', req.body);
   model.createMessage(req.body)
   .then((message) => {
-    res.status(200).json(message);
+    res.status(201).json(message);
   })
   .catch((serverErr) => {
     res.status(500).send(serverErr);
   })
 })
 
-
+router.get('/:userId', (req, res) => {
+  console.log('req.query: ', req.query);
+  model.getUserMessages(req.params.userId, req.query.receiver_id)
+  .then((messages) => {
+    res.status(200).json(messages);
+  })
+  .catch((serverErr) => {
+    res.status(500).send(serverErr);
+  })
+})
 
 module.exports = router;
