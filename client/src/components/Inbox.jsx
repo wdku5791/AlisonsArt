ed7228a0c2260b01cf7e0c01d9b307afa3a12056
boybutcher <BYRONdeLARGE@yahoo.com>
2017-04-24 17:18:19 -0700

    grabs last message between 2 users, doesnt work reliablly yet for connecting to chat

--- client/src/components/Inbox.jsx
+++ client/src/components/Inbox.jsx
@@ -6,15 +6,11 @@ import * as ChatActions from './../actions/chatActionCreator.jsx';
 class Inbox extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      loaded: []
-    }
     this.getInbox = this.getInbox.bind(this);
     this.renderChatRoom = this.renderChatRoom.bind(this);
   }
 
   getInbox() {
-    console.log('getInbox, userId: ', this.props.userId);
     fetch(`/messages/inbox/${this.props.userId}`, {
       method: 'GET',
       headers: new Headers({
@@ -30,8 +26,23 @@ class Inbox extends React.Component {
     })
     .then((data) => {
       console.log('inbox SUCCESS! data: ', data);
+      let filteredInbox = {};
+      for (var i = 0; i < data.length; i++) {
+        var userId = this.props.userId;
+        var senderId = data[i].sender_id;
+        var receiverId = data[i].receiver_id;
+        if (userId !== senderId && !filteredInbox[senderId]) {
+          filteredInbox[senderId] = data[i];
+        } else if (userId !== receiverId && !filteredInbox[receiverId]) {
+          filteredInbox[receiverId] = data[i];
+        }
+      }
+      var filteredArray = [];
+      for (var key in filteredInbox) {
+        filteredArray.push(filteredInbox[key]);
+      }
       let { dispatch } = this.props;
-      dispatch(ChatActions.getInbox(data));
+      dispatch(ChatActions.getInbox(filteredArray));
     })
     .catch((error) => {
       console.log('getInbox FAILED! error: ', error);
@@ -80,24 +91,18 @@ class Inbox extends React.Component {
         <ul>
           {
             this.props.inboxMessages.map(conversation => {
-              if ((this.state.loaded.indexOf(conversation.sender_id)) === -1 || (this.state.loaded.indexOf(conversation.receiver_id)) === -1) {
-                if (this.props.userId === conversation.sender_id) {
-                  this.state.loaded = this.state.loaded.slice().concat(conversation.receiver_id);
-                  console.log('this.state.loaded: ', this.state.loaded);
-                  return (
-                    <li value={conversation.receiver_id} onClick={this.renderChatRoom}>
-                      <strong>With: {conversation.receiver_id}</strong> "{conversation.text}" <strong>at: {conversation.message_date}</strong>
-                    </li>
-                  )
-                } else {
-                  this.state.loaded = this.state.loaded.slice().concat(conversation.sender_id);
-                  console.log('this.state.loaded: ', this.state.loaded);
-                  return (
-                    <li value={conversation.sender_id} onClick={this.renderChatRoom}>
-                      <strong>With: {conversation.receiver_id}</strong> "{conversation.text}" <strong>at: {conversation.message_date}</strong>
-                    </li>
-                  )
-                }
+              if (this.props.userId !== conversation.sender_id) {
+                return (
+                  <li value={conversation.sender_id} onClick={this.renderChatRoom}>
+                    <strong>Conversation With: {conversation.sender_id} </strong>"{conversation.text}"<strong> at: {conversation.message_date}</strong>
+                  </li>
+                )
+              } else {
+                return (
+                  <li value={conversation.sender_id} onClick={this.renderChatRoom}>
+                    <strong>Conversation With: {conversation.sender_id} </strong>"{conversation.text}"<strong> at: {conversation.message_date}</strong>
+                  </li>
+                )
               }
             })
           }
