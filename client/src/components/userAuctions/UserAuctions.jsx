6e238594028e9b0ff290dfeda7e36c0c6f84f758
Anthony Bianco <anthony.bianco3@gmail.com>
2017-04-26 20:19:52 -0700

    render errors, loading, and empty state messages

--- client/src/components/userAuctions/UserAuctions.jsx
+++ client/src/components/userAuctions/UserAuctions.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Image, Grid } from 'semantic-ui-react';
+import { Container, Grid, Message, Icon, Button } from 'semantic-ui-react';
 import * as userAuctions from '../../actions/userAuctionsActionCreator';
 import ClosedAuction from './ClosedAuction.jsx';
 import OpenAuction from './OpenAuction.jsx';
@@ -8,6 +8,9 @@ import { connect } from 'react-redux';
 class UserAuctions extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showError: false
+    };
   }
 
   componentWillMount() {
@@ -22,26 +25,58 @@ class UserAuctions extends Component {
     })
     .then(response => {
       if (!response.ok) {
-        throw Error(response.statusText);
+        const error = new Error(response.statusText);
+        error.status = response.status;
+        throw error;
       }
-        dispatch(userAuctions.fetchingUserAuctions(false));
-        return response.json();
+      dispatch(userAuctions.fetchingUserAuctions(false));
+      return response.json();
     })
     .then(data => {
-      console.log(data);
       dispatch(userAuctions.userAuctionsFetched(data));
     })
     .catch((err) => dispatch(userAuctions.userAuctionsErrored(err)));
   }
 
+  showErrorToggle() {
+    this.setState({
+      showError: !this.state.showError
+    });
+  }
+
+  goToAuctions() {
+    this.props.history.push('/auctions');
+  }
+
   render() {
-    let { openAuctions, closedAuctions, isFetching } = this.props.userAuctions;
+    let { openAuctions, closedAuctions, isFetching, hasErrored } = this.props.userAuctions;
     const { history } = this.props;
-    let closedAuctionsDiv = null;
 
     if (isFetching) {
       return (
-        <div>Loading~~~~~</div>
+        <Message icon>
+          <Icon name="circle notched" loading />
+          <Message.Content>
+            <Message.Header>Just a second!</Message.Header>
+            Your Auctions are on the way.
+          </Message.Content>
+        </Message>
+      );
+    }
+
+    if (hasErrored) {
+      return (
+        <Container>
+          <Message
+            header="Something Went Wrong!"
+            content="There's been an error loading this page. How about checking out some art and coming back in a minute?"
+          />
+          <a onClick={() => this.showErrorToggle()}>Details</a>
+          <Message
+            hidden={!this.state.showError}
+            content={`${error.status}: ${error.message}`}
+          />
+        </Container>
       );
     }
 
@@ -56,7 +91,14 @@ class UserAuctions extends Component {
                 history={history}
               />
               )
-            ) : ''}
+            ) : 
+            <Container>
+              <Message
+                header="You're not Bidding on any art"
+                content="You dream piece is waiting for you. Click below to find it."
+              />
+              <Button onClick={() => this.goToAuctions()}>Go to Auctions</Button>
+            </Container>}
           </Grid.Row>
         </Grid>
         <h3>Ongoing Auctions</h3>
@@ -68,7 +110,14 @@ class UserAuctions extends Component {
                 auction={auction}
               />
               )
-            ) : ''}
+            ) :
+            <Container>
+              <Message
+                header="You have won any auctions... Yet"
+                content="You dream piece is waiting for you. Click below to find it."
+              />
+              <Button onClick={() => this.goToAuctions()}>Go To Auctions</Button>
+            </Container>}
           </Grid.Row>
         </Grid>
       </div>
