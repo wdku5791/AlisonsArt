03e745a7200e6b61f3dcecd744b864839e93a363
Anthony Bianco <anthony.bianco3@gmail.com>
2017-04-27 12:11:36 -0700

    add photo and emails to contact us page

--- client/src/components/ContactUs.jsx
+++ client/src/components/ContactUs.jsx
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import { Image, Grid, Container } from 'semantic-ui-react';
 
 class ContactUs extends Component {
 
   render() {
-    return(
-      <div>
-        Contact us at: xxxxx
-      </div>
+    return (
+      <Container>
+        <h1 className="contactHeader">Tenacious Turtles</h1>
+        <Grid stackable>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <Image
+                src="assets/turtles.jpg"
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <h4 className="imageHeader">Alison Zhang</h4>
+              <a href="mailto:alisonzthu@gmail.com"><p>alisonzthu@gmail.com</p></a>
+              <h4 className="imageHeader">Bryan Nguyen</h4>
+              <a href="mailto:bryan.nguyen.a@gmail.com"><p>bryan.nguyen.a@gmail.com</p></a>
+              <h4 className="imageHeader">Winston Ku</h4>
+              <a href="mailto:wdku5791@gmail.com"><p>wdku5791@gmail.com</p></a>
+              <h4 className="imageHeader">Anthony Bianco</h4>
+              <a href="mailto:anthony.bianco3@gmail.com"><p>anthony.bianco3@gmail.com</p></a>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
     );
   }
 }
